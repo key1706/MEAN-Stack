e378cb95b78993c0b28536d7992a05490a0054ec
key <quockhuong0330@gmail.com>
2017-08-21 17:56:13 +0700

    Auth App update 21/08/2017

--- angular-scr/src/app/app.module.ts
+++ angular-scr/src/app/app.module.ts
@@ -11,18 +11,19 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 import {ValidateService} from "./services/validate.service";
 import {AuthService} from "./services/auth.service";
 import {FlashMessagesModule} from "angular2-flash-messages";
-import { FooterComponent } from './components/footer/footer.component';
+import {AuthGuard} from "./guards/auth.guard";
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'profile', component: ProfileComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
 ]
 
 @NgModule({
@@ -43,7 +44,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService, AuthService],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
